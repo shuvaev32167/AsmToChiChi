4b0533a34082b7cb1289520ad3a4a023fb67f0fb
shuvaev32167 <shuvaev32167@gmail.com>
2014-02-18 19:41:24 +0400

    равка в move

--- asmtobin.cpp
+++ asmtobin.cpp
@@ -58,7 +58,57 @@ void AsmToBin::asmToBin()
                             throw QString("Не верно казан адресс памяти у MOVE");
                 }
 //                size = nakop.size();
-                for (char i = 0; i < 4; ++i)
+                for (char i = 2; i < 4; ++i)
+                {
+                    if (nakop[i] == '0')
+                        binText+="0000";
+                    else
+                        if (nakop[i] == '1')
+                            binText+="0001";
+                        else
+                            if (nakop[i]=='2')
+                                binText+="0010";
+                            else
+                                if (nakop[i] == '3')
+                                    binText+="0011";
+                                else
+                                    if (nakop[i]=='4')
+                                        binText+="0100";
+                                    else
+                                        if (nakop[i] == '5')
+                                            binText+="0101";
+                                        else
+                                            if (nakop[i]=='6')
+                                                binText+="0110";
+                                            else
+                                                if (nakop[i] == '7')
+                                                    binText+="0111";
+                                                else
+                                                    if (nakop[i]=='8')
+                                                        binText+="1000";
+                                                    else
+                                                        if (nakop[i] == '9')
+                                                            binText+="1001";
+                                                        else
+                                                            if (nakop[i]=='A')
+                                                                binText+="1010";
+                                                            else
+                                                                if (nakop[i] == 'B')
+                                                                    binText+="1011";
+                                                                else
+                                                                    if (nakop[i]=='C')
+                                                                        binText+="1100";
+                                                                    else
+                                                                        if (nakop[i] == 'D')
+                                                                            binText+="1101";
+                                                                        else
+                                                                            if (nakop[i]=='E')
+                                                                                binText+="1110";
+                                                                            else
+                                                                                if (nakop[i] == 'F')
+                                                                                    binText+="1111";
+                }
+                for (char i = 0; i < 2; ++i)
                 {
                     if (nakop[i] == '0')
                         binText+="0000";
